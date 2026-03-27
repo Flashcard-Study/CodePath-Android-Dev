bf4647348324bee034157a1ba3218a572ab07673
rrojas1819 <rrojas181920@gmail.com>
2026-03-27 14:13:46 -0400

    Integrated changes into dev

--- app/src/main/java/com/example/flashcardstudy/ui/addcard/AddCardFragment.kt
+++ app/src/main/java/com/example/flashcardstudy/ui/addcard/AddCardFragment.kt
@@ -1,18 +1,6 @@
 package com.example.flashcardstudy.ui.addcard
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.flashcardstudy.R
 
-class AddCardFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.fragment_add_card, container, false)
-    }
-}
+class AddCardFragment : Fragment(R.layout.fragment_add_card)
