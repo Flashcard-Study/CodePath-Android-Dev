8d0bdbe579af12feadce1c46946d92a398a9d9b1
rrojas1819 <rrojas181920@gmail.com>
2026-03-27 13:34:50 -0400

    App Navigation & Empty Dashboard plus Baseplate for everything

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,6 +39,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("androidx.fragment:fragment-ktx:1.5.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/example/flashcardstudy/ui/deckdetail/DeckDetailFragment.kt
@@ -0,0 +1,6 @@
+package com.example.flashcardstudy.ui.deckdetail
+
+import androidx.fragment.app.Fragment
+import com.example.flashcardstudy.R
+
+class DeckDetailFragment : Fragment(R.layout.fragment_deck_detail)
